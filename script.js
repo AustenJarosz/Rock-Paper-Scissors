7f753be330e0b86b8cc2d1d7a76e361bf2049ed3
AustenJarosz <austenjarosz@gmail.com>
2024-09-05 13:40:37 -0500

    Added several functions: humanScore(), computerScore(), and getHumanChoice()

--- script.js
+++ script.js
@@ -3,15 +3,32 @@ function getComputerChoice(){
     let choice = ""
     let randNumber = Math.random()
     if(randNumber < 0.33){
-        choice = "Rock"
+        choice = "rock"
     }
     else if (randNumber > 0.66){
-        choice = "Paper"
+        choice = "paper"
         }
         else{
-            choice = "Scissors"
+            choice = "scissors"
         }
         console.log(choice);
 }
 
+function getHumanChoice(){
+    let intExit = 0;
+    while(intExit == 0){
+    let choice = prompt("Do you want to go with rock, paper, or scissors?");
+    choice = choice.toLowerCase();
+    if(choice === "rock" || choice === "paper" || choice === "scissors" || choice == "1"){
+        intExit = 1;
+    }
+    else{
+        alert('invalid choice. Please try again or type "1" to end');
+        
+    }
+
+}
+}
+
 getComputerChoice();
+getHumanChoice();
